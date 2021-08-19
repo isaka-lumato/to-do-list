1b29c29fd13855fdde026ad6b6f8f1462ca2362d
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-08-19 16:19:09 +0300

    structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const arr = [
+  {
+    description: 'do exercises before lunch',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'complete all assignments',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'clean my house',
+    completed: false,
+    index: 2,
+  },
+];
+const listParent = document.querySelector('#all-tasks');
 
- // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const display = () => {
+  listParent.innerHTML = '';
+  arr.forEach((task) => {
+    const list = `
+    <div class="eachTask">
+      <div class="group-list">
+      <input type="checkbox" class="box" id="list-box" name="list-box">
+        <p class="task-name">${task.description}</p>
+      </div> 
+      <button class="menu-icon" id="${task.index}"><i class="fas fa-ellipsis-v"></i></button>
+    </div>
+    <hr>`;
+    listParent.innerHTML += list;
+  });
+};
 
-  return element;
-}
-
-document.body.appendChild(component());
+window.onload = display();
